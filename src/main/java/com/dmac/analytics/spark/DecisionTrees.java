b7dfb9e86a751c6d589d417878dd71a83027fcec
lohith kumar <lbasavaraju-consultant@scholastic.com>
2015-09-18 21:26:32 +0530

    Decision Tree Example with some minor errors

--- src/main/java/com/dmac/analytics/spark/DecisionTrees.java
+++ src/main/java/com/dmac/analytics/spark/DecisionTrees.java
@@ -1,8 +1,22 @@
 package com.dmac.analytics.spark;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.spark.SparkConf;
+
+import scala.Tuple2;
+
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.ml.feature.HashingTF;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.mllib.tree.DecisionTree;
 import org.apache.spark.mllib.tree.model.DecisionTreeModel;
@@ -21,18 +35,91 @@ public class DecisionTrees {
 		
 		
 		JavaSparkContext sc = new JavaSparkContext(sparkConfig);
-		JavaRDD<String> inputData = sc.textFile("/Users/tester/golf.csv");
+		JavaRDD<LabeledPoint> data = sc.textFile("file:///Users/tester/Desktop/gaga.csv").map(new Function<String, LabeledPoint>() {
+
+			@Override
+			public LabeledPoint call(String v1) throws Exception {
+                List<String> featureList = Arrays.asList(v1.trim().split(",")); 
+                
+                String placement = featureList.get(0).toLowerCase();
+                String prominence = featureList.get(1).toLowerCase();
+                String pricing = featureList.get(2).toLowerCase();
+                String eyeLevel = featureList.get(3).toLowerCase();
+                String customerPurchase = featureList.get(4).toLowerCase();
+                
+                double placementQualifier = 0.0;
+                double prominenceQualifer = 0.0;
+                double pricingQualifer = 0.0;
+                double eyeLevelQualifier = 0.0;
+                double custPurchaseQualifier = 0.0;
+                
+                if (placement.equals("end_rack"))
+                	placementQualifier = 1;
+                else if (placement.equals("cd_spec"))
+                	placementQualifier = 2;
+                else if(placement.equals("std_rack"))
+                	placementQualifier = 3;
+                
+                prominenceQualifer = Double.parseDouble(prominence);   
+                pricingQualifer = Double.parseDouble(pricing);
+                
+                if(eyeLevel.equals("true"))
+                	eyeLevelQualifier = 1;
+                else 
+                	eyeLevelQualifier = 0;
+                
+                if(customerPurchase.equals("yes"))
+                	custPurchaseQualifier = 1;
+                else
+                	custPurchaseQualifier = 0;
+                
+                return new LabeledPoint(custPurchaseQualifier, Vectors.dense(prominenceQualifer,pricingQualifer,eyeLevelQualifier,placementQualifier)); 
+			}
+			
+			
+		});
 		
-		inputData.foreach(z -> System.out.println(z));
+	/*	String datapath = "/Users/tester/Desktop/gaga.csv";
+		JavaRDD<LabeledPoint> data = MLUtils.loadLabeledData(sc.sc(),datapath).toJavaRDD(); */
+		
+		JavaRDD<LabeledPoint>[] splits = data.randomSplit(new double[]{0.7, 0.3});
+		JavaRDD<LabeledPoint> trainingData = splits[0];
+		JavaRDD<LabeledPoint> testData = splits[1];
+		
+		Map<Integer, Integer> categoricalFeaturesInfo = new HashMap<Integer, Integer>();
+		
+		
+		data.foreach(z -> System.out.println(z));
 		//MLUtils.de
 		
-		JavaRDD<LabeledPoint> source  = null;
+//		JavaRDD<LabeledPoint> source  = null;
 		String impurity = "gini";
-		int maxDepth = 5;
+		int maxDepth = 15;
 		int maxBins = 32;
 		
 		
-		DecisionTreeModel dtm = DecisionTree.trainClassifier(source, 2, null, impurity, maxDepth, maxBins);
+		DecisionTreeModel dtm = DecisionTree.trainClassifier(trainingData, 2, categoricalFeaturesInfo, impurity, maxDepth, maxBins);
+		
+		JavaPairRDD<Double, Double> predictionAndLabel =
+				  testData.mapToPair(new PairFunction<LabeledPoint, Double, Double>() {
+				    @Override
+				    public Tuple2<Double, Double> call(LabeledPoint p) {
+				      return new Tuple2<Double, Double>(dtm.predict(p.features()), p.label());
+				    }
+				  });
+		
+		Double testErr =
+				  1.0 * predictionAndLabel.filter(new Function<Tuple2<Double, Double>, Boolean>() {
+				    @Override
+				    public Boolean call(Tuple2<Double, Double> pl) {
+				      return !pl._1().equals(pl._2());
+				    }
+				  }).count() / testData.count();
+				System.out.println("Test Error: " + testErr);
+				System.out.println("Learned classification tree model:\n" + dtm.toDebugString());
+
+		dtm.save(sc.sc(), "myModelPath");
+		DecisionTreeModel sameModel = DecisionTreeModel.load(sc.sc(), "myModelPath");
 		
 	}
 	
