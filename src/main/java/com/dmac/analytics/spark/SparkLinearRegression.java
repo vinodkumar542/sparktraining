28498bf8ff0fdd4d18a460a0f8431451527bda5b
Chinnasamy <chinnasamyad@gmail.com>
2015-09-25 15:52:05 +0530

    Linear regression and spark recoverable checkpointing

---
+++ src/main/java/com/dmac/analytics/spark/SparkLinearRegression.java
@@ -0,0 +1,17 @@
+package com.dmac.analytics.spark;
+
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.mllib.regression.LinearRegressionWithSGD;
+
+public class SparkLinearRegression {
+
+	public static void main(String[] args) {
+		
+		LinearRegressionWithSGD lr = new LinearRegressionWithSGD();
+		
+		LinearRegressionModel lk = null;
+		//lk.predict();
+
+	}
+
+}
