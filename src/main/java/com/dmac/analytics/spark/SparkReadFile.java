e755732963eac0f3ec4113cd17360a2467f3e704
Chinnasamy <chinnasamyad@gmail.com>
2015-12-04 12:27:16 +0530

    Added in the winutils to the code base. Also added in the code to show how to avoid in the winutils exception.

--- src/main/java/com/dmac/analytics/spark/SparkReadFile.java
+++ src/main/java/com/dmac/analytics/spark/SparkReadFile.java
@@ -7,20 +7,17 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkReadFile {
 	
-	
-	
 	public static void main(String[] args) {
-
+		
+		
 		SparkConf sparkConfig = new SparkConf()
 						.setAppName("ReadLogFile")
 						.setMaster("local[8]");
 						//.setMaster("spark://Apples-MacBook-Pro.local:7077");
-						
-
 		
 		JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConfig);
 
-		javaSparkContext.textFile("file:///Users/apple/titanic3.csv")
+		javaSparkContext.textFile("file:///E:/ac/spark/code/sparktraining/data/titanic3.csv")
 						//.foreach(System.out::println);
 						.foreach(z -> System.out.println(z));
 		
